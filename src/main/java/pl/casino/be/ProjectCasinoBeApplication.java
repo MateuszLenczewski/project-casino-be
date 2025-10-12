ec6e24278fb35cf236fa7ac07e09df681a5bbdaa
Mateusz Lenczewski <mateuszejo@gmail.com>
2025-10-12 23:01:23 +0200

    Initial commit

---
+++ src/main/java/pl/casino/be/ProjectCasinoBeApplication.java
@@ -0,0 +1,13 @@
+package pl.casino.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCasinoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCasinoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/casino/be/ProjectCasinoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.casino.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCasinoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
